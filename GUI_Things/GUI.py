93e32ff5402a01a790a91bbb2382aaa7f7fa0d3e
Randy Kaneer <s11306958@students.duvalschools.org>
2023-04-17 20:46:51 -0400

    v0.1

---
+++ GUI_Things/GUI.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+def hello():
+    value = entry.get()
+    out["text"] = f"Hello, {value}"
+
+window = tk.Tk()
+
+label = tk.Label(text = "Enter Name: ", height = 2, width = 25)
+entry = tk.Entry(width = 25)
+button = tk.Button(text = "Go!", command = hello)
+out = tk.Label(height = 2, width = 25)
+
+label.pack()
+entry.pack()
+button.pack()
+out.pack()
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
